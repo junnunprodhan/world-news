c4039bff770eade9e49893dc110ba8fb0c439c69
junnun15 <junnunprodhan@gmail.com>
2022-09-02 14:50:58 -0700

    dynamic card added

--- js/app.js
+++ js/app.js
@@ -13,16 +13,66 @@ const displayData=(newsCategories)=>{
        const list = document.createElement('div');
        list.classList.add('bg');
        list.innerHTML =`
-      <a class="nav-link" href"a"> ${newsCategory.category_name}</a>`;
+      <a onclick="loadNews('${newsCategory.category_id}')" class="nav-link" href"a"> ${newsCategory.category_name}</a>`;
        categoriesContainer.appendChild(list);
     });
 }
 
-newsDataLoad()
+const loadNews=(id)=>{
+    const url =`https://openapi.programming-hero.com/api/news/category/${id}`;
+    fetch(url)
+    .then (res=> res.json())
+    .then (data=> displayNews(data.data))
+}
 
-document.getElementById('show-all-categories').addEventListener('click',function(even){
-    console.log(even.target.innerText)
-})
+const displayNews=(news)=>{
+    console.log(news)
+  const cardContainer =document.getElementById('card-container');
+  cardContainer.textContent='';
+  news.forEach(news => {
+    const {image_url,thumbnail_url,title,total_view} = news;
+    console.log(news.author.img)
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('row', 'g-0');
+    cardInfo.innerHTML=`
+    <div class="col-md-4 mt-4">
+                    <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${title}</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <div class="d-flex justify-content-around">
+                        <div class="d-flex">
+                            <div>
+                                <img style="width:50px; height:50px" src="${news.author.img}" alt="">
+                            </div>
+                            <div class="ms-2">
+                                <h5>${news.author.name ? news.author.name :'Not Found'}</h5>
+                                <p>22-09-23</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h4>1.M</h4>
+                        </div>
+                        <div>
+                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailsModal">Show Details
+                            </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+    `
+    cardContainer.appendChild(cardInfo);
+  });
+
+}
+
+
+newsDataLoad()
+// document.getElementById('show-all-categories').addEventListener('click',function(even){
+//     console.log(even.target.innerText)
+// })
 
 
 
